3e6c3f226a938f544c290d3068615cbb8ff7e136
bernd <berndroth0@gmail.com>
2023-01-05 09:29:56 +0100

    * new string

--- app/src/main/java/at/co/netconsulting/balancesheet/MainActivity.java
+++ app/src/main/java/at/co/netconsulting/balancesheet/MainActivity.java
@@ -174,7 +174,7 @@ public class MainActivity extends BaseActivity {
                     listView.setAdapter(adapter);
                  adapter.notifyDataSetChanged();
                  alertDialog.setView(rowList);
-                 alertDialog.setPositiveButton("Return", new DialogInterface.OnClickListener() {
+                 alertDialog.setPositiveButton(R.string.button_return, new DialogInterface.OnClickListener() {
                      @Override
                      public void onClick(DialogInterface dialog, int which) {
                          refreshAndRequestOutputFromDatabase(false);
